94f1bc0d9d0e75c67a1de3356bc5ea7cc146d32a
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2023-06-14 00:53:36 +0300

    ADDED: header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { ChatBubbleLeftRightIcon, PhoneIcon } from "@heroicons/react/24/solid"
 
-const Header = () => {
+const Header = ({ chat }) => {
     return (
-        <div>Header</div>
+        <div className='chat-header'>
+            <div className='flexbetween'>
+                <ChatBubbleLeftRightIcon className='icon-chat' />
+                <h3 className='header-text'>{chat.title}</h3>
+            </div>
+            <div className='flexbetween'>
+                <PhoneIcon className='icon-phone' />
+                {chat.description !== "⬅️ ⬅️ ⬅️" ? (<p className='header-text'>{chat.description}</p>) : (<p className='header-text'>no chat selected</p>)}
+            </div>
+        </div>
     )
 }
 
